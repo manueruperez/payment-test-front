a75c570399141b879295ee81a45ec80f23ba1eb0
manueru <juanperez@gcflatino.com>
2024-02-03 17:53:21 -0500

    feat(layout): header and footer added to layout

--- src/layouts/main-layout.vue
+++ src/layouts/main-layout.vue
@@ -1,5 +1,22 @@
 <template>
-  <nav class="header">
-    <p>soy le header</p>
-  </nav>
+  <div>
+    <MainHeader />
+    <router-view />
+    <MainFooter />
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import MainHeader from "./main-header.vue";
+import MainFooter from "./main-footer.vue";
+export default defineComponent({
+  components: { MainHeader, MainFooter },
+  data() {
+    return {
+      layout: "layout",
+      initUserInfo: false,
+    };
+  },
+});
+</script>
