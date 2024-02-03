83de4cffbced55343dd3dccd0da6b6d025a697a3
manueru <juanperez@gcflatino.com>
2024-02-03 18:27:29 -0500

    feat(NoContent):component created

---
+++ src/components/shared/NoContent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="no-content d-flex justify-content-center align-items-center">
+    <div class="text-center">
+      <!-- To do -->
+      <!-- <img
+        src="@/assets/no-content.png"
+        alt="No Content"
+        class="img-fluid"
+        style="max-width: 200px"
+      /> -->
+      <p class="mt-3">{{ message }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NoContent",
+  props: {
+    message: {
+      type: String,
+      default: "No hay contenido disponible.",
+    },
+  },
+});
+</script>
+
+<style scoped>
+.no-content {
+  min-height: 300px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <NoContent message="Holi" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import NoContent from "@/components/shared/NoContent.vue";
 @Options({
   components: {
-    HelloWorld,
+    NoContent,
   },
 })
 export default class HomeView extends Vue {}
