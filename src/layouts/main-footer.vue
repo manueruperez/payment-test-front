a6adfc15bff44f9e1563e4d6884bb49e5e6d0e86
manueru <juanperez@gcflatino.com>
2024-02-05 12:39:50 -0500

    feat(mainFooter): show own brand

--- src/layouts/main-footer.vue
+++ src/layouts/main-footer.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="navbar navbar-expand-lg navbar-light bg-light">
-    <p class="m-auto">made whit 🤍 by manueruperez</p>
+    <p class="m-auto">
+      made whit 🤍 by
+      <a href="https://manueruperez.github.io/resilience-coder/" target="_blank"
+        >manueruperez
+      </a>
+    </p>
   </div>
 </template>
